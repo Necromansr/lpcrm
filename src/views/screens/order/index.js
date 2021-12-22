f855133a29eaf415e83c6fce336108b13defb5b3
Necromansr <maximkerimov88@gmail.com>
2021-12-22 12:06:21 +0200

    Update index.js

--- src/views/screens/order/index.js
+++ src/views/screens/order/index.js
@@ -977,7 +977,7 @@ function Order({ data, rowHeight, visibleRows, navigation, changeStart, changeEn
 
     setTimeout(() => {
       changeStart(Math.floor(e.target.scrollTop / rowHeight));
-      changeEnd(Math.min((data.length - visible - 1), Math.floor(e.target.scrollTop / rowHeight + visible + 1)))
+      changeEnd((Math.min((data.length - visible - 1), Math.floor(e.target.scrollTop / rowHeight + visible + 1))))
     }, 100);
 
 
