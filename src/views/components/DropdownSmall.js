28e95a1f0656b465692e706d704330d3a05b0acd
Necromansr <maximkerimov88@gmail.com>
2022-01-18 12:26:57 +0200

    Update DropdownSmall.js

--- src/views/components/DropdownSmall.js
+++ src/views/components/DropdownSmall.js
@@ -38,8 +38,6 @@ class DropdownSmall extends Component {
             this.setState({
                 select: false
             })
-            document.getElementById("tooltipBtn").style.animation = '';
-
         }
 
         if ((this.props.refresh !== prevProps.refresh)) {
@@ -58,12 +56,6 @@ class DropdownSmall extends Component {
     }
 
     open = (e) => {
-        if (!this.props.wrapper && this.state.select) {
-            document.querySelectorAll('.simplebar-content-wrapper').forEach(x => x.scrollTo({
-                top: 0,
-                behavior: "smooth"
-            }))
-        }
 
         if (!this.props.wrapper) {
             this.setState({
@@ -83,7 +75,6 @@ class DropdownSmall extends Component {
         this.setState({
             open: false
         })
-        document.getElementById("tooltipBtn").style.animation = '';
         if (!this.props.wrapper)
             this.props.setRange(true)
         
@@ -97,7 +88,6 @@ class DropdownSmall extends Component {
         if (index === 0) {
             arr[index].select = !arr[index].select;
             arr.slice(1).forEach(x => x.select = false)
-            document.getElementById("tooltipBtn").style.animation = '';
             this.props.onWrapper(false);
             this.setState({ arr: [...arr], select: false, open: false })
             return;
@@ -174,30 +164,7 @@ class DropdownSmall extends Component {
                         </div>
                     </div>
                 </div>
-                <div className={(((this.state.open || this.state.sort !== "") && (!(this.state.arr.filter(x => x.select === true && x?.text !== 'Все').length > 0) || this.state.open)) || (this.state.select && this.props.wrapper)) ? "sort-btn sort-toggle" : "sort-btn"}  onClick={this.onClick}
-                    
-                //     onMouseEnter={e => {
-                //     timer = setTimeout(() => {
-
-                //         document.getElementById("tooltipBtn").style.fontSize = '11px';
-                //         document.getElementById("tooltipBtn").innerText = 'Сортировать данные ↑↓';
-                //         let posElement = e.target.getBoundingClientRect();
-                //         document.getElementById("tooltipBtn").style.left = posElement.x + "px";
-                //         document.getElementById("tooltipBtn").style.top = posElement.y + 18 + "px";
-                //         document.getElementById("tooltipBtn").style.animation = 'delay-btn 0.20s forwards';
-                //         let blockWidth = posElement.width;
-                //         let screenWidth = document.body.clientWidth;
-                //         let widthTooltip = document.getElementById("tooltipBtn").offsetWidth;
-                //         if (screenWidth < posElement.x + widthTooltip + blockWidth) {
-                //             document.getElementById("tooltipBtn").style.left = posElement.x - widthTooltip + 'px';
-                //         }
-                //     }, 200)
-                // }}
-                //     onMouseLeave={e => {
-                //         clearTimeout(timer);
-                //         document.getElementById("tooltipBtn").style.animation = '';
-                //     }}
-                >
+                <div className={(((this.state.open || this.state.sort !== "") && (!(this.state.arr.filter(x => x.select === true && x?.text !== 'Все').length > 0) || this.state.open)) || (this.state.select && this.props.wrapper)) ? "sort-btn sort-toggle" : "sort-btn"}  onClick={this.onClick}>
                     <svg style={this.state.sort === 'up' ? { transform: 'scaleY(-1)' } : {}} width="10" height="10" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3.37459 0.240197L0 3.06626L1.14931 4.49643L3.07879 2.83706L3.07655 12H4.90818L4.91062 2.83589L6.84264 4.49525L7.99196 3.0608L4.61609 0.240197C4.21951 -0.079919 3.77147 -0.080212 3.37459 0.240197ZM9.16119 8.15695C9.65816 8.15695 10.0603 7.74553 10.0603 7.23743C10.0603 6.72932 9.65816 6.3179 9.16119 6.3179H7.08288V8.15695H9.16119ZM10.6748 11.5357C11.1716 11.5357 11.5739 11.1243 11.5739 10.6162C11.5739 10.1081 11.1716 9.69679 10.6748 9.69679H7.08298V11.5357H10.6748Z" fill="black"></path>
                     </svg>
